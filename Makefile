ff9bf123e9d121898f9817a40f91f5bca194d700
Benjamin Stürz <benni@stuerz.xyz>
2021-09-17 19:54:28 +0200

    added support for readline

--- Makefile
+++ Makefile
@@ -3,10 +3,18 @@ sources = $(wildcard src/*.hs)
 GHC = ghc -isrc -no-keep-hi-files -no-keep-o-files
 PREFIX ?= /usr/local
 
+# Disable if you don't have the readline package install
+# See also: <https://hackage.haskell.org/package/readline>
+ENABLE_READLINE ?= 1
+
+ifeq ($(ENABLE_READLINE),1)
+GHCFLAGS += -lreadline -DENABLE_READLINE=1
+endif
+
 all: haskalc
 
 haskalc: $(sources)
-	$(GHC) -o $@ $(sources)
+	$(GHC) -o $@ $(sources) $(GHCFLAGS)
 
 clean:
 	rm -f haskalc
--- src/main.hs
+++ src/main.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE CPP #-}
+#if ENABLE_READLINE
+import System.Console.Readline
+#endif
+
+import System.Exit
 import System.IO
 import Parsing
 import Eval
@@ -7,11 +13,20 @@ run Nothing          = "Error"
 run (Just e)         = show $ evalExpr e
 
 main :: IO()
+#if ENABLE_READLINE
+main =   do
+         maybeLine <- readline "% "
+         case maybeLine of
+            Nothing     -> exitSuccess
+            Just "exit" -> exitSuccess
+            Just line   -> putStrLn $ run $ parse_expr line
+         main
+#else
 main =   do
          putStr "> "
          hFlush stdout
-         str   <- getLine
-         if str == [] then putStr "" else do
-            putStrLn $ run $ parse_expr str
+         line   <- getLine
+         if line == [] then putStr "" else do
+            putStrLn $ run $ parse_expr line
             main
-         
+#endif
